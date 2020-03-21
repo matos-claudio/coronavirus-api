cf9088bdf3c21c6ed0b535184706020abaaa3ee0
Claudio Matos~ <caupath16@gmail.com>
2020-03-21 00:22:19 -0300

    Organizando pastas

--- src/routes/test/test.route.js
+++ src/routes/test/test.route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-let testController = require('../../controller/test.controller')
+let testController = require('../../controller/test/test.controller')
 router.get('/example/', testController.exampleGetRequest)
 
 module.exports = router
