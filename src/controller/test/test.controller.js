171adffdbe223dbc77e12627201afa86611d5521
Claudio Matos~ <caupath16@gmail.com>
2020-03-21 00:24:04 -0300

    Organizando pastas

--- src/controller/test/test.controller.js
+++ src/controller/test/test.controller.js
@@ -1,3 +1,3 @@
 exports.exampleGetRequest = (req, res) => {
-    res.status(200).send({ status: 200, message: 'API em funcionamento' })
+    res.status(200).send({ status: 200, message: 'API em funcionamento', version: '1.0.0' })
 }
